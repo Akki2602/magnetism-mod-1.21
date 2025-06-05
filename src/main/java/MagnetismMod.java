12d0298e7cb5d94744a40850bfd47133baf61dda
Akki2602 <akshat.sharma26022010@gmail.com>
2025-06-05 20:49:25 +0530

    Add new folders and files for Magnetism Mod

---
+++ src/main/java/MagnetismMod.java
@@ -0,0 +1,24 @@
+package net.akki.magnetismmod;
+
+import net.akki.magnetismmod.block.ModBlocks;
+
+import net.akki.magnetismmod.item.ModItemGroups;
+import net.akki.magnetismmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MagnetismMod implements ModInitializer {
+	public static final String MOD_ID = "magnetismmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+
+	}
+}
