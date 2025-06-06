bba4a176edb24befaa056eda0e74b48ee568ace1
Akki2602 <96011879+Akki2602@users.noreply.github.com>
2025-06-06 20:11:19 +0530

    Update MagnetUtils.java

--- src/main/java/net/akki/magnetismmod/util/MagnetUtils.java
+++ src/main/java/net/akki/magnetismmod/util/MagnetUtils.java
@@ -1,21 +1,23 @@
 package net.akki.magnetismmod.util;
 
 import java.util.List;
-import net.minecraft.util.ActionResult;
-import net.minecraft.world.World;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 
 public class MagnetUtils {
-    public static void attractItems(class_1937 world, class_2338 pos, double range, double strength) {
-        List<class_1542> items = world.method_8390(class_1542.class,
-                new net.minecraft.class_238(pos).method_1014(range),
-                item -> item.method_5805());
 
-        for (class_1542 item : items) {
-            class_243 pull = class_243.method_24953(pos).method_1020(item.method_19538()).method_1029().method_1021(strength);
-            item.method_18799(item.method_18798().method_1019(pull));
-            item.field_6037 = true;
+    public static void attractItems(World world, BlockPos pos, double range, double strength) {
+        List<ItemEntity> items = world.getEntitiesByClass(ItemEntity.class,
+                new Box(pos).expand(range),
+                ItemEntity::isAlive);
+
+        for (ItemEntity item : items) {
+            Vec3d pull = Vec3d.ofCenter(pos).subtract(item.getPos()).normalize().multiply(strength);
+            item.setVelocity(item.getVelocity().add(pull));
+            item.velocityModified = true;
         }
     }
 }
