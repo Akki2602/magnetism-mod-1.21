16b9c2a65fd1f26025af3ac7cb90955945f59ad2
Akki2602 <96011879+Akki2602@users.noreply.github.com>
2025-06-06 20:18:06 +0530

    Update MagnetismMod.java

--- src/main/java/net/akki/magnetismmod/MagnetismMod.java
+++ src/main/java/net/akki/magnetismmod/MagnetismMod.java
@@ -9,6 +9,8 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import net.minecraft.util.Identifier;
+
 public class MagnetismMod implements ModInitializer {
 	public static final String MOD_ID = "magnetismmod";
 
