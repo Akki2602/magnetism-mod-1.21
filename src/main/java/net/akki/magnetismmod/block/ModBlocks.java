32ba478615a68706535bddd53927c56e8d81fdbc
Akki2602 <96011879+Akki2602@users.noreply.github.com>
2025-06-06 20:05:09 +0530

    Update ModBlocks.java

--- src/main/java/net/akki/magnetismmod/block/ModBlocks.java
+++ src/main/java/net/akki/magnetismmod/block/ModBlocks.java
@@ -3,40 +3,34 @@ package net.akki.magnetismmod.block;
 import net.akki.magnetismmod.MagnetismMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Item.Settings;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
 
+    public static final Block MAGNET_ORE = registerBlock("magnet_ore",
+            new Block(AbstractBlock.Settings.copy(Blocks.IRON_ORE).strength(3f)));
 
+    public static final Block MAGNET_DEEPSLATE_ORE = registerBlock("magnet_deepslate_ore",
+            new Block(AbstractBlock.Settings.copy(Blocks.DEEPSLATE_IRON_ORE).strength(4f)));
 
-    public static final class_2248 MAGNET_ORE = registerBlock("magnet_ore",
-            new class_2431(class_6019.method_35017(2, 5),
-                    class_4970.class_2251.method_9637().method_9632(3f).method_29292()));
-
-
-    public static final class_2248 MAGNET_DEEPSLATE_ORE = registerBlock("magnet_deepslate_ore",
-            new class_2431(class_6019.method_35017(3, 6),
-                    class_4970.class_2251.method_9637().method_9632(4f).method_29292().method_9626(class_2498.field_29033)));
-
-    private static class_2248 registerBlock(String name, class_2248 block){
+    private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
-        return class_2378.method_10230(class_7923.field_41175, class_2960.method_60655(MagnetismMod.MOD_ID, name), block);
+        return Registry.register(Registries.BLOCK, new Identifier(MagnetismMod.MOD_ID, name), block);
     }
 
-
-    private static void registerBlockItem(String name, class_2248 block) {
-        class_2378.method_10230(class_7923.field_41178, class_2960.method_60655(MagnetismMod.MOD_ID, name),
-                new class_1747(block, new class_1792.class_1793()));
+    private static void registerBlockItem(String name, Block block) {
+        Registry.register(Registries.ITEM, new Identifier(MagnetismMod.MOD_ID, name),
+                new BlockItem(block, new Item.Settings()));
     }
+
     public static void registerModBlocks() {
-    MagnetismMod.LOGGER.info("Registering Mod Blocks for " + MagnetismMod.MOD_ID);
-
-
-
+        MagnetismMod.LOGGER.info("Registering Mod Blocks for " + MagnetismMod.MOD_ID);
     }
 }
