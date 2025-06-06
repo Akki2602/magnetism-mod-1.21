71a3533789d1dff9211ad021a27f70cbdf6f064f
Akki2602 <96011879+Akki2602@users.noreply.github.com>
2025-06-06 20:18:21 +0530

    Update MagnetismModClient.java

--- src/main/java/net/akki/magnetismmod/MagnetismModClient.java
+++ src/main/java/net/akki/magnetismmod/MagnetismModClient.java
@@ -2,6 +2,8 @@ package net.akki.magnetismmod;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import net.minecraft.util.Identifier;
+
 public class MagnetismModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
